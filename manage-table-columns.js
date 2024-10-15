e767b408c1b602216bd13a487fdca158dc4b9f81
priya_lipl <priyabrata.senapati@lipl.in>
2024-10-15 18:31:15 +0530

    init design

--- manage-table-columns.js
+++ manage-table-columns.js
@@ -1,29 +1,123 @@
-function openManageColumns(e){if($(".manage-columns.in").length>0)$(".manage-columns").removeClass("in"),setTimeout(()=>{$(".manage-columns").remove(),$(document).off("click")},350);else{let n=JSON.parse(window.localStorage.getItem(e)||"[]");$(".manage-columns").remove(),$("body").prepend('<div class="manage-columns"><h3>Manage Table Header</h3><ul class="column-list"></ul></div>');let t=0;$(e).find("thead tr:first-child th").each(function(){let a=$(this).hasClass("fix"),l=!n.includes(t),i="<li "+(a&&"disabled")+'><label><input type="checkbox" name="columns" value="'+t+'" onchange="prModifyTable(this, '+t+", '"+e+`')" ${l?"checked":""} />`+this.innerText+"</label></li>";$(".manage-columns .column-list").append(i),t++}),$(document).on("click",function(e){!$(e.target).closest(".manage-columns").length&&$(".manage-columns").hasClass("in")&&($(".manage-columns").removeClass("in"),setTimeout(()=>{$(".manage-columns").remove(),$(document).off("click")},350))}),setTimeout(()=>{$(".manage-columns").addClass("in")},100)}}function prModifyTable(e,n,t){let a=JSON.parse(window.localStorage.getItem(t)||"[]");e.checked?($(t).find("tr th:nth-child("+(n+1)+")").show(),$(t).find("tr td:nth-child("+(n+1)+")").show(),a=a.filter(e=>e!==n)):($(t).find("tr th:nth-child("+(n+1)+")").hide(),$(t).find("tr td:nth-child("+(n+1)+")").hide(),a.push(n)),window.localStorage.setItem(t,JSON.stringify(a))}$(document).ready(function(){let e=`<style id="pr-manage-columns" type="text/css">
-    .manage-columns {
-        position: fixed;
-        z-index: 2000;
-        background: #fff;
-        padding: 15px;
-        width: 280px;
-        height: 100vh;
-        top: 0;
-        transition: all 0.3s;
-        bottom: 0;
-        overflow: auto;
-        right: -280px;
-        box-shadow: 0px 0px 20px #3333;
+/**********************************
+ * Create Button onclick function
+ * <button type="button" onclick="openManageColumns(tableID)">Manage Columns</button>
+ */
+// TABLE COLUMN MANAGE
+//============================
+$(document).ready(function(){
+    const style = `<style id="pr-manage-columns" type="text/css">
+        .manage-columns {
+            position: fixed;
+            z-index: 2000;
+            background: #fff;
+            padding: 15px;
+            width: 280px;
+            height: 100vh;
+            top: 0;
+            transition: all 0.3s;
+            bottom: 0;
+            overflow: auto;
+            right: -280px;
+            box-shadow: 0px 0px 20px #3333;
+        }
+        .manage-columns.in {right:0}
+        .manage-columns .column-list{
+            list-style: none;
+            padding: 0;
+            margin: 0;
+        }
+        .manage-columns h3 {
+            font-size: 20px;
+            text-align: center;
+            padding: 5px;
+            border-bottom: 2px solid;
+        }
+        .manage-columns [disabled]{pointer-events: none; opacity: 0.6;}
+        .manage-columns .column-list label{font-size: 16px; position: relative; padding: 5px; display: block; user-select: none;}
+        .manage-columns .column-list label input{display: none;}
+        .manage-columns .column-list label b::before {
+            font-weight: 700;
+            display: inline-block;
+            margin-right: 10px;
+            line-height: 1;
+        }
+        .manage-columns .column-list label input:checked + b::before {
+            content: '\\2713';
+            color: #00cf00;
+        }
+        .manage-columns .column-list label input:not(:checked) + b::before {
+            content: '\\10102';
+            color: #cf0000;
+            margin-right: 14px;
+        }
+    </style>`;
+    $('head').append(style);
+    $('table').each(function(){
+        const id = '#'+this.id;
+        let columnList = window.localStorage.getItem(id) || '[]';
+        let colArr = JSON.parse(columnList);
+
+        console.log(id, colArr);
+        // const isChecked = !colArr.includes(index);
+        for(let i=0; i<colArr.length; i++){
+            $(id).find('tr th:nth-child('+(colArr[i]+1)+')').hide();
+            $(id).find('tr td:nth-child('+(colArr[i]+1)+')').hide();
+        }
+    })
+});
+
+function openManageColumns(s){
+    if($('.manage-columns.in').length > 0){
+        $('.manage-columns').removeClass('in');
+        setTimeout(() => {
+            $('.manage-columns').remove();
+            $(document).off('click'); // Remove the event handler to prevent memory leaks
+        }, 350);
+    }else{
+        let columnList = window.localStorage.getItem(s) || '[]';
+        let colArr = JSON.parse(columnList);
+        $('.manage-columns').remove();
+        $('body').prepend('<div class="manage-columns"><h3>Manage Table Header</h3><ul class="column-list"></ul></div>');
+        let index = 0;
+        $(s).find('thead tr:first-child th').each(function(){
+            const isFixed = $(this).hasClass('fix');
+            const isChecked = !colArr.includes(index);
+            const li = `<li `+(isFixed && 'disabled')+`><label><input type="checkbox" name="columns" value="`+index+`" onchange="prModifyTable(this, `+index+`, '`+s+`')" ${isChecked ? 'checked' : ''} /><b></b>`+ this.innerText +`</label></li>`;
+            $('.manage-columns .column-list').append(li);
+            index++;
+        });
+
+        // Detect click outside of the .manage-columns element
+        $(document).on('click', function(event) {
+            // alert($('.manage-columns').hasClass('in'))
+            if (!$(event.target).closest('.manage-columns').length && $('.manage-columns').hasClass('in')) {
+                $('.manage-columns').removeClass('in');
+                setTimeout(() => {
+                    $('.manage-columns').remove();
+                    $(document).off('click'); // Remove the event handler to prevent memory leaks
+                }, 350);
+            }
+        });
+
+        setTimeout(() => {
+            $('.manage-columns').addClass('in');
+        }, 100);
     }
-    .manage-columns.in {right:0}
-    .manage-columns .column-list{
-        list-style: none;
-        padding: 0;
-        margin: 0;
+}
+
+function prModifyTable(d,i,s){
+    let columnList = window.localStorage.getItem(s) || '[]';
+    let colArr = JSON.parse(columnList);
+
+    if(!d.checked){
+        $(s).find('tr th:nth-child('+(i+1)+')').hide();
+        $(s).find('tr td:nth-child('+(i+1)+')').hide();
+        colArr.push(i);
+    }else{
+        $(s).find('tr th:nth-child('+(i+1)+')').show();
+        $(s).find('tr td:nth-child('+(i+1)+')').show();
+        colArr = colArr.filter(colIndex => colIndex !== i);
     }
-    .manage-columns h3 {
-        font-size: 20px;
-        text-align: center;
-        padding: 5px;
-        border-bottom: 2px solid;
-    }
-    .manage-columns [disabled]{pointer-events: none; opacity: 0.6;}
-</style>`;$("head").append(e),$("table").each(function(){let e="#"+this.id,n=JSON.parse(window.localStorage.getItem(e)||"[]");console.log(e,n);for(let t=0;t<n.length;t++)$(e).find("tr th:nth-child("+(n[t]+1)+")").hide(),$(e).find("tr td:nth-child("+(n[t]+1)+")").hide()})});
+
+    window.localStorage.setItem(s, JSON.stringify(colArr));
+}
